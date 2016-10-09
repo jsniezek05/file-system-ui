498740242b7799b722fe2bf8c7b5303446e9e8c5
John Sniezek <sniezekjp@aol.com>
2016-10-09 17:02:51 -0400

    Revert "jp work"
    This reverts commit fdb9c51b7913cce8bfa0dda4833f251fee7138fb.

--- src/code-mirror.js
+++
@@ -1,8 +0,0 @@
-export class CodeMirrorService {
-	getInstance() {
-		this.instance = this.instance || CodeMirror(document.getElementById('code'), {
-            lineNumbers: true
-        });
-        return this.instance;
-	}
-}
